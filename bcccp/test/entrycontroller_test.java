39d46ba2ff1576e9bb3b5359d4448ffad6dfccd0
Geeth Herath <30451534+geethknsg@users.noreply.github.com>
2017-09-24 16:02:01 +1000

    EntryController.java
    EntryController

--- bcccp/test/entrycontroller_test.java
+++ bcccp/test/entrycontroller_test.java
@@ -1,14 +1,38 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package bcccp.test;
+// import bcccp.carpark.entry.EntryController;
+/////**
 
-/**
- *
- * @author Geeth
- */
+//import bcccp.carpark.Carpark;
+//import bcccp.carpark.ICarSensor;
+//import bcccp.carpark.ICarSensorResponder;
+//import bcccp.carpark.IGate;
+import bcccp.carpark.entry.EntryController;
+//import bcccp.carpark.entry.IEntryController;
+//import bcccp.carpark.entry.IEntryUI;
+//import bcccp.tickets.adhoc.IAdhocTicket;
+//import bcccp.tickets.adhoc.IAdhocTicketDAO;
+//import bcccp.tickets.season.ISeasonTicket;
+//import bcccp.tickets.season.ISeasonTicketDAO;
+//import java.util.List;
+
+// *
+// * @author Geeth
+// */
 public class entrycontroller_test {
+   
+public static void main ( String [] args)   {
     
+    EntryController ec = new EntryController();
+     ec.carEventDetected("park001", true);
+     ec.log("take a ticket");
+     //ec.ticketTaken();
+     ec.carEventDetected("par11", true);
+    // ec.buttonPushed();
+ }
 }
